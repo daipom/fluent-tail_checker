b1fa828ce45f46a869cd1fa241cc03c9507294cb
Daijiro Fukuda <fukuda@clear-code.com>
2025-05-07 14:13:29 +0900

    initial commit
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

---
+++ test/fluent/tail_checker_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Fluent::TailCheckerTest < Test::Unit::TestCase
+  test "VERSION" do
+    assert do
+      ::Fluent::TailChecker.const_defined?(:VERSION)
+    end
+  end
+
+  test "something useful" do
+    assert_equal("expected", "actual")
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "fluent/tail_checker"
+
+require "test-unit"
