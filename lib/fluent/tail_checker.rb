50562b915870a7bc7829916aa1b428fb33447886
Daijiro Fukuda <fukuda@clear-code.com>
2025-05-07 14:42:41 +0900

    add initial exe
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

--- lib/fluent/tail_checker.rb
+++ lib/fluent/tail_checker.rb
@@ -4,7 +4,8 @@ require_relative "tail_checker/version"
 
 module Fluent
   module TailChecker
-    class Error < StandardError; end
-    # Your code goes here...
+    def self.run
+      puts Fluent::TailChecker::VERSION
+    end
   end
 end
