83243cc1d77a294601eb776cbc73d24d7cbdebd6
Daijiro Fukuda <fukuda@clear-code.com>
2025-05-08 21:13:15 +0900

    test

--- test/script/command-test.bat
+++ test/script/command-test.bat
@@ -1,5 +1,14 @@
-tailcheck --version || exit /b 1
-tailcheck --help || exit /b 1
-tailcheck test/data/pos_normal || exit /b 1
-tailcheck test/data/pos_normal test/data/pos_duplicate_unwatched_path  && exit /b 1
-tailcheck --follow_inodes test/data/pos_follow_inodes_normal || exit /b 1
+@echo on
+call tailcheck --version || exit /b 1
+
+@echo on
+call tailcheck --help || exit /b 1
+
+@echo on
+call tailcheck test/data/pos_normal || exit /b 1
+
+@echo on
+call tailcheck test/data/pos_normal test/data/pos_duplicate_unwatched_path && exit /b 1
+
+@echo on
+call tailcheck --follow_inodes test/data/pos_follow_inodes_normal || exit /b 1
