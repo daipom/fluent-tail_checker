84a8e258ecbba762b95739bcf3b68e0313547dbf
Daijiro Fukuda <fukuda@clear-code.com>
2025-05-08 21:27:28 +0900

    CI: support gem test for Windows
    Signed-off-by: Daijiro Fukuda <fukuda@clear-code.com>

---
+++ test/script/command-test.bat
@@ -0,0 +1,14 @@
+@echo on
+call tailcheck --version || exit /b 1
+
+@echo on
+call tailcheck --help || exit /b 1
+
+@echo on
+call tailcheck test/data/pos_normal || exit /b 1
+
+@echo on
+call tailcheck test/data/pos_normal test/data/pos_duplicate_unwatched_path && exit /b 1
+
+@echo on
+call tailcheck --follow_inodes test/data/pos_follow_inodes_normal || exit /b 1
